ea131584892ce92d81aae8a971b42c998ea408b5
ayseayse0346 <ayseayse.ayse0346@gmail.com>
2024-03-16 15:44:19 +0300

    axios

--- JS-20-coin_app/coin_app_nodejs/main.js
+++ JS-20-coin_app/coin_app_nodejs/main.js
@@ -1,28 +1,24 @@
 
 import "./scss/style.scss"
+import { getCoins } from "./src/getCoins"
 
-import { getCoins } from "./src/getCoins" 
+//? Selectors
 
-// ?selectors
 const form = document.querySelector("header form")
-// console.log(form);
 
-// ? form için submit event'nin tanımlanması
-form.addEventListener("submit",(e)=>{
-// console.log(e);
-e.preventDefault()//!form eventinin tüm doğal davranışını disable eder.
-getCoinData()
-e.target.reset() //?formu silme davranışını geri getirir.
+//? Form için submit event'nin tanimlanmasi
+form.addEventListener("submit", (e) => {
+  e.preventDefault() //! form eventinin tüm dogal davranilari disable eder.
+  getCoinData()
+  e.target.reset() //? formu silme davranisini aktif hale getir.
 })
 
- const getCoinData = ()=>{
+const getCoinData = () => {
   const input = document.querySelector("header form input").value
-  // console.log(input.value);
   if (!input.trim()) {
-    alert("ınput must be entered")
-  }else{
-    //?GET COIN
+    alert("Input must be entered")
+  } else {
+    //? GET COIN
     getCoins(input)
   }
-
- }
+}
--- JS-20-coin_app/coin_app_nodejs/src/getCoins.js
+++ JS-20-coin_app/coin_app_nodejs/src/getCoins.js
@@ -1,32 +1,64 @@
-import displayCoinCard from "./displayCoins";
+
+// // ?export default ile paylaşıldığı için süslüsüz ve herhangi bir isimle import edilebildi.
+// import displayCoins from "./displayCoins"
+// import axios from "axios"
 
 
-// ?named export
- export const getCoins =async (keyword) => {
-    const API_KEY = "coinrankingd3f7cfe1685f6c65a427f956670de2e135194d74952d5ad7"
-    //  const URL = 'https://api.coinranking.com/v2/coin/Qwsogvtv82FCd' //bu veri bütün  coinleri getiriyor.
-    const URL = 'https://api.coinranking.com/v2/coin/Qwsogvtv82FCd?search=${keyword}'
-    const options = {
-        headers: {
-          'Content-Type': API_KEY,
-          // 'x-access-token': 'your-access-token-here',
-        },
-      };
-     try {
-const res = await fetch(URL, options)
-//    console.log(res);ham veriye burdan ulaşırız.o yüzden jasonlaştırırız.
-const json = await res.json()
-console.log(json.data.coins[0]);
-if(!json.data.coins[0]){
-    alert("coin can not be found")
-}else{
-    displayCoinCard()  //bu fonksiyonu displaycoins kalsöründe oluştu. yukarda importladık.
-}
-     } catch (error) {
-        console.log(error);
-     }
+// // ?named export
+//  export const getCoins =async (keyword) => {
+//     const API_KEY = "coinrankingd3f7cfe1685f6c65a427f956670de2e135194d74952d5ad7"
+//     //  const URL = 'https://api.coinranking.com/v2/coin/Qwsogvtv82FCd' //bu veri bütün  coinleri getiriyor.
+//     const URL = 'https://api.coinranking.com/v2/coin/Qwsogvtv82FCd?search=${keyword}'
+//     const options = {
+//         headers: {
+//           'Content-Type': API_KEY,
+//           // 'x-access-token': 'your-access-token-here',
+//         },
+//       };
+//      try {
+// const res = await fetch(URL, options)
+// //    console.log(res);ham veriye burdan ulaşırız.o yüzden jasonlaştırırız.
+// const json = await res.json()
+// // console.log(json.data.coins[0]); 
+// if(!json.data.coins[0]){
+//     alert("coin can not be found")
+// }else{
+//     displayCoins(json.data.coins[0])  //bu fonksiyonu displaycoins kalsöründe oluştu. yukarda importladık.
+// }
+//      } catch (error) {
+//         console.log(error);
+//      }
 
      
 
 
+// }
+//? export default ile payalşıldığı için süslü parantezsi ve herhangi bir isimle import edilebildi.
+import displayCoins from "./displayCoins"
+import axios from "axios"
+
+//? named export
+export const getCoins = async (keyword) => {
+  const URL = `https://api.coinranking.com/v2/coins?search=${keyword}`
+
+  //   const options = {
+  //     headers: { "x-access-token": API_KEY },
+  //   }
+
+  const options = {
+    headers: { "x-access-token": import.meta.env.VITE_API_KEY },
+  }
+
+  try {
+    const res = await axios(URL, options)
+    // console.log(res.data.data.coins[0])
+
+    if (!res.data.data.coins[0]) {
+      alert("Coin can not be found")
+    } else {
+      displayCoins(res.data.data.coins[0])
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
